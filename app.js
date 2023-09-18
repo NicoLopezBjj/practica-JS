7adbc5eeea63fb03e3ad70a6ef2e57b3c1017faf
Nico <lopeznnr@gmail.com>
2023-09-18 13:20:27 -0300

    add obtenerDatos promise.all

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Cargar Datos Asíncronamente:
 //     Crea una función cargarDatos que simule la carga de datos de un servidor usando setTimeout. Utiliza async/await para esperar la carga de los datos y luego imprime el resultado.
+  
 
-function simularCarga (){
+    function simularCarga (){
     return new Promise((resolve)=>{
         setTimeout(()=>{
             resolve('Datos cargados correctamente')
@@ -21,3 +22,73 @@ async function cargarDatos () {
 
 cargarDatos()
 
+  // Múltiples Promesas:
+    // Crea una función obtenerDatos que tome un arreglo de URLs y utilice Promise.all para cargar múltiples datos de forma concurrente. Utiliza async/await para esperar la resolución de todas las promesas.
+
+const urls = ['https://api1.com','https://api2.com','https://api3.com']
+
+  
+
+async function obtenerDatos(urls) { 
+    try{
+        console.log('Cargando datos..')
+        const promesas = urls.map(url => simularCarga(url))
+        const resultado = await Promise.all(promesas)
+        console.log(resultado)
+        return resultado
+    } catch (error) {
+        console.log('Error al cargar los datos', error)
+        throw error
+    }
+}
+
+
+(async () => {
+    try {
+        const datos = await obtenerDatos(urls);
+        console.log('Datos cargados con exito');
+        datos.forEach(dato => console.log(dato));
+    } catch (error) {
+        console.error ('Error al cargar los datos',error);
+    }
+})();
+
+
+
+
+
+
+// function simularCarga (url){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             const exito = Math.random() < 0.8
+//             if (exito) {
+//                 resolve (`Datos cargados de ${url}`)
+//             } else {
+//                 reject (`Error al cargar datos de ${url}`)
+//             }
+//         },2000)
+//     })
+// }
+
+// async function obtenerDatos () { 
+//     try{
+//         console.log('Cargando datos..')
+//         const promesas = urls.map (url => simularCarga(url))
+//         const resultado = await Promise.all(promesas)
+//         console.log(resultado)
+//     } catch (error) {
+//         console.log('Error al cargar los datos', error)
+//     }
+// }
+
+
+
+
+
+// obtenerDatos(urls)
+//     .then(datos => {
+//         console.log('Datos cargados con exito')
+//         datos.forEach(dato => console.log(dato))
+//     })
+//     .catch(error => console.error ('Error al cargar los datos',error))
